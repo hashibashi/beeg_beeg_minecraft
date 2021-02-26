e2c0d56ae02d4a54007cdfa98cb54228dd1c172b
Jay <jay_patel_cool@yahoo.com>
2021-02-26 03:33:20 -0500

    add conf

---
+++ render_conf.py
@@ -0,0 +1,19 @@
+outputdir = 'output_dir'
+
+worlds['Beeg Beeg Minecraft'] = \
+    r"C:\Users\USER\Downloads\Beeg Beeg Minecraft\Beeg Beeg Realm (Beeg world)"
+
+renders['day'] = {
+    'world': 'Beeg Beeg Minecraft',
+    'rendermode': 'smooth_lighting',
+    'title': 'Daytime Render',
+    'imgformat': 'webp',
+    }
+renders['night'] = {
+    'world': 'Beeg Beeg Minecraft',
+    'rendermode': 'smooth_night',
+    'title': 'Night Render',
+    'imgformat': 'webp',
+    }
+
+defaultzoom = 17
